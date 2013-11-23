e3569c6e888070d43164e58301768fa4eaa22f5f
My Name <parisliahyun@gmail.com>
2013-11-23 16:44:29 -0500

    working code on sending messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,37 @@
 Turkey::Application.routes.draw do
   devise_for :users
   resources :users, only: :show do
-     post :generate_new_password_email
-  end
+    post :generate_new_password_email
 
-  get 'message/compose', to: 'messages#new', as: :compose
-
-  resources :messages do
-    collection do
-      get 'index', to: 'messages#index', as: :index
-      get 'sent', to: 'messages#sent', as: :sent
-      post 'reply', to: 'messages#reply', as: :reply
-      post 'trash', to: 'messages#trash', as: :trash
+    resources :messages do
+      member do
+        # post :create
+        post :new
+        post :reply
+        post :trash
+        post :empty_trash
+      end
     end
+
+    # get 'message/compose', to: 'messages#new', as: :compose
+
+    # resources :messages do
+    #   collection do
+    #     get 'index', to: 'messages#index', as: :index
+    #     get 'sent', to: 'messages#sent', as: :sent
+    #     post 'reply', to: 'messages#reply', as: :reply
+    #     post 'trash', to: 'messages#trash', as: :trash
+    #   end
+    # end
+
+    # post 'mailbox/empty_trash', to: 'mailboxes#empty_trash', as: :empty_trash
+    # get 'mailbox/:mailbox', to: 'mailboxes#show', as: :mailbox
+    # get 'mailbox/:mailbox/:id', to: 'messages#show', as: :mailbox_message
+
   end
 
-  post 'mailbox/empty_trash', to: 'mailboxes#empty_trash', as: :empty_trash
-  get 'mailbox/:mailbox', to: 'mailboxes#show', as: :mailbox
-  get 'mailbox/:mailbox/:id', to: 'messages#show', as: :mailbox_message
+
+
 
   # resources :dishes
   root 'welcome#index'
