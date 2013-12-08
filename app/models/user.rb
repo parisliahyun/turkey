7559253f25a5fe00b84ba0eb41080712a7902bbe
My Name <parisliahyun@gmail.com>
2013-12-08 15:01:23 -0800

    working code on ratings

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   validates :name, :email, presence: true
   
-  :letsrate_rater
-  :letsrate_rateable
+  letsrate_rater
+  letsrate_rateable
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
